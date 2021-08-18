79e30f772f3512508422b60465643a047352ef27
JeanHeberth <jeanheberth19@gmail.com>
2021-08-18 10:28:03 -0300

    Subindo o inicio do projeto

---
+++ src/test/java/TesteGoogle.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteGoogle {
+
+	private  WebDriver navegadorGoogle;
+	private  WebDriver navegadorFirefox;
+
+
+	@Test
+	public void teste() {
+
+		// Driver Firefox
+		System.setProperty("webdriver.gecko.driver", "D:\\Documentos\\DriversTestes\\geckodriver.exe");
+		navegadorFirefox = new FirefoxDriver();
+
+		// Driver do google
+		//System.setProperty("webdriver.chrome.driver", "D:\\Documentos\\DriversTestes\\chromedriver.exe");
+		//navegadorGoogle = new ChromeDriver();
+		navegadorFirefox.get("http://www.google.com");
+		Assert.assertEquals("Google", navegadorFirefox.getTitle());
+
+	}
+
+}
